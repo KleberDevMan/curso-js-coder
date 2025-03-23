0302d553f66f1c2fd5bf75af24b1c27223fd8047
kleber-chaves-smartfit <kleber.chaves@bioritmo.com.br>
2025-03-23 17:44:11 -0300

    learning: review control structures

--- exercicios-web/controle/if2.js
+++ exercicios-web/controle/if2.js
@@ -1,21 +1,21 @@
 function teste1(num) {
-    // if sem {} --> executa apenas uma linha de código
-    if(num > 7)
-        console.log(num)
-    
-    console.log('Final')
+  // if sem {} --> executa apenas uma linha de código
+  if (num > 7)
+    console.log(num)
+
+  console.log('Final')
 }
 
 teste1(6)
 teste1(8)
 
 function teste2(num) {
-    // aqui o ponto e vírgula significa uma instrução vazia.
-    if(num > 7); 
-    
-    { // cuidado com o ';', não usar com as estruturas de controle
-        console.log(num)
-    }
+  // aqui o ponto e vírgula significa uma instrução vazia.
+  if (num > 7) ;
+
+  { // cuidado com o ';', não usar com as estruturas de controle
+    console.log(num)
+  }
 }
 
 teste2(6)
--- exercicios-web/controle/switch.js
+++ exercicios-web/controle/switch.js
@@ -1,24 +1,30 @@
 // Nao esqueça de colocar o Break
 const imprimirResultado = function (nota) {
-    switch (Math.floor(nota)) {
-        case 10:
-        case 9:
-            // caso seja 10 ou 9
-            console.log('Quadro de Honra')
-            break
-        case 8: case 7:
-            // caso seja 8 ou 7
-            console.log('Aprovado')
-            break
-        case 6: case 5: case 4:
-            console.log('Recuperação')
-            break
-        case 3: case 2: case 1: case 0:
-            console.log('Reprovado')
-            break
-        default:
-            console.log('Nota inválida')
-    }
+  switch (Math.floor(nota)) {
+    case 10:
+    case 9:
+      // caso seja 10 ou 9
+      console.log('Quadro de Honra')
+      break
+    case 8:
+    case 7:
+      // caso seja 8 ou 7
+      console.log('Aprovado')
+      break
+    case 6:
+    case 5:
+    case 4:
+      console.log('Recuperação')
+      break
+    case 3:
+    case 2:
+    case 1:
+    case 0:
+      console.log('Reprovado')
+      break
+    default:
+      console.log('Nota inválida')
+  }
 }
 
 imprimirResultado(10)
