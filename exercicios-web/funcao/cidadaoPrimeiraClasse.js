a8ecf81c77047454346bac313e35eaca55c31b50
KleberDevMan <seuemail@exemplo.com>
2025-03-27 22:44:49 -0300

    learning: function is citizen first-class

--- exercicios-web/funcao/cidadaoPrimeiraClasse.js
+++ exercicios-web/funcao/cidadaoPrimeiraClasse.js
@@ -4,32 +4,40 @@
 // Função como dado
 
 // criar de forma literal
-function fun1() { }
+function fun1() {
+}
 
 // Armazenar em uma variável
-const fun2 = function () { }
+const fun2 = function () {
+}
 
 // Armazenar em um array
-const array = [function (a, b) { return a + b }, fun1, fun2]
+const array = [function (a, b) {
+  return a + b
+}, fun1, fun2]
 console.log(array[0](2, 3))
 
 // Armazenar em um atributo de objeto
 const obj = {}
-obj.falar = function () { return 'Opa' }
+obj.falar = function () {
+  return 'Opa'
+}
 console.log(obj.falar())
 
 // Passar função como param
 function run(fun) {
-    fun()
+  fun()
 }
 
-run(function () { console.log('Executando...') })
+run(function () {
+  console.log('Executando...')
+})
 
 // Um função pode retornar/conter um função
 function soma(a, b) {
-    return function (c) {
-        console.log(a + b + c)
-    }
+  return function (c) {
+    console.log(a + b + c)
+  }
 }
 
 soma(2, 3)(4)
