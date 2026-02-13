654439db185dc3204f59f7bbcecee55508c74bab
kleber.chaves <kleber.chaves@bioritmo.com.br>
2026-02-12 22:33:24 -0300

    learning: improve code formatting and update timer intervals in entradaESaida.js and temporizador.js; enable product listing in servidor.js

--- exercicios-web/node/temporizador.js
+++ exercicios-web/node/temporizador.js
@@ -1,22 +1,26 @@
 const schedule = require('node-schedule')
 
-const tarefa1 = schedule.scheduleJob('*/5 * 12 * * 2', function () {
+// a cada 5 segundos
+const tarefa1 = schedule.scheduleJob('*/1 * * * * *', function () {
     console.log('Executando Tarefa 1!', new Date().getSeconds())
 })
 
 setTimeout(function () {
     tarefa1.cancel()
     console.log('Cancelando Tarefa 1!')
-}, 20000)
+}, 10000)
 
 // setImmediate
 // setInterval
 
 const regra = new schedule.RecurrenceRule()
-regra.dayOfWeek = [new schedule.Range(1, 5)]
-regra.hour = 12
-regra.second = 30
+regra.second = new schedule.Range(0, 59, 5)
 
 const tarefa2 = schedule.scheduleJob(regra, function () {
     console.log('Executando Tarefa 2!', new Date().getSeconds())
 })
+
+setTimeout(function () {
+    tarefa2.cancel()
+    console.log('Cancelando Tarefa 2!')
+}, 20000)
