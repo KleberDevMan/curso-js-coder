36373040cdd63f42a7f9af1fb156f729a5b3aca5
KleberDevMan <kjjunio80@gmail.com>
2026-08-17 21:59:10 -0300

    feat: add NavItem component for navigation links

---
+++ exercicios-web/react/crud/frontend/src/components/template/NavItem.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import {Link} from "react-router-dom";
+
+export default props =>
+    <Link to={props.to}>
+        <i className={props.icon}></i> {props.label}
+    </Link>
